c5d8bf20e5552fdeffa2ba382e95ed0d37065aa5
luongvantam <luongtammc11@gmail.com>
2026-06-14 13:49:26 +0700

    update ios

--- patch2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("/Users/luongtoi/Documents/Projects/CasioEmuMsvc/CasioEmuMsvc/CasioEmuMsvc/src/casioemu.cpp", "r") as f:
-    content = f.read()
-
-content = content.replace("emulator.return_to_home_requested = true;", "emulator.return_to_home_requested = true;\\n					emulator.Shutdown();")
-
-with open("/Users/luongtoi/Documents/Projects/CasioEmuMsvc/CasioEmuMsvc/CasioEmuMsvc/src/casioemu.cpp", "w") as f:
-    f.write(content)
--- patch_emu.py
+++
@@ -1,7 +0,0 @@
-import re
-with open("/Users/luongtoi/Documents/Projects/CasioEmuMsvc/CasioEmuMsvc/CasioEmuMsvc/src/Emulator.hpp", "r") as f:
-    content = f.read()
-
-content = content.replace("std::atomic<bool> screenshot_requested{};", "std::atomic<bool> screenshot_requested{};\\n		std::atomic<bool> screenshot_taken{false};")
-with open("/Users/luongtoi/Documents/Projects/CasioEmuMsvc/CasioEmuMsvc/CasioEmuMsvc/src/Emulator.hpp", "w") as f:
-    f.write(content)
